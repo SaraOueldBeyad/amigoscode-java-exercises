fd086d3d125907e74ef1441e17b81e45292284d9
Sara Oueld Beyad <oueldbeyad.sara@gmail.com>
2026-08-28 06:40:45 -0400

    _3_TypeCasting

--- src/main/java/com/amigoscode/_1_beginners/_3_TypeCasting.java
+++ src/main/java/com/amigoscode/_1_beginners/_3_TypeCasting.java
@@ -10,35 +10,47 @@ package com.amigoscode._1_beginners;
 public class _3_TypeCasting {
 
     public static void main(String[] args) {
-
         // TODO: 1 - Widen an int to a double (implicit casting)
-        // Declare an int variable with any value, then assign it to a double variable.
+        int a = 20;
+        double b = a;
         // Print both variables to see the result.
+        System.out.println(a);
+        System.out.println(b);
 
 
         // TODO: 2 - Narrow a double to an int (explicit casting)
-        // Declare a double variable (e.g., 9.78), then cast it to an int.
+        double c  = 9.78;
+        int d = (int)c;
         // Print both variables to see what happens to the decimal part.
+        System.out.println(c);
+        System.out.println(d);
 
 
         // TODO: 3 - Cast an int to a char to get the character it represents
+        int num = 65;
+        char e = (char)num;
         // Hint: int value 65 corresponds to 'A' in ASCII
         // Print the resulting char.
-
+        System.out.println(e);
 
         // TODO: 4 - Cast a char to an int to get its ASCII value
+        char character = 'Z';
+        int n = character;
         // Hint: char 'Z' has an ASCII value of 90
         // Print the resulting int.
+        System.out.println(n);
 
 
         // TODO: 5 - Convert a String "42" to an int using Integer.parseInt()
+        String s = "42";
         // Declare a String variable with the value "42", then parse it to an int.
         // Print the result.
-
+        System.out.println(Integer.parseInt(s));
 
         // TODO: 6 - Convert an int 42 to a String using String.valueOf()
+        int v = 42;
         // Declare an int variable with the value 42, then convert it to a String.
         // Print the result.
-
+        System.out.println(String.valueOf(v));
     }
 }
